c3b449f5e2e2fdfbecff71a87ccbc9bdf43a0775
Mucenicalexandru <mucenic.alexandru@gmail.com>
2020-04-01 11:14:42 +0300

    Completed def

--- kata2.py
+++ kata2.py
@@ -1,3 +1,12 @@
-string = "phone"
-
-print(len(string))
+def get_middle(s):
+    0 < len(s) < 1000
+    if len(s) % 2 != 0:
+        x = len(s)
+        y = (x - 1) / 2
+        return s[int(y)]
+    else:
+        q = len(s)
+        w = int((q - 1) / 2)
+        r = int((q + 1) / 2)
+        string = s[w]+s[r]
+        return string
